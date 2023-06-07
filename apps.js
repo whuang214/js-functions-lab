f12e8d9df2cdacad4e3b1d318004bcd0166941bc
whuang214 <whuang2146@gmail.com>
2023-06-07 16:16:42 -0400

    completed all the functions

---
+++ apps.js
@@ -0,0 +1,75 @@
+/* JavaScript Functions Lab */
+
+// 1.
+function maxOfTwoNumbers(x, y) {
+    return  x >= y ? x : y;
+}
+console.log(maxOfTwoNumbers(99, 100)); // expect 100
+
+// 2.
+function maxOfThree(x, y, z) {
+    return x >= y && x >= z ? x : y >= x && y >= z ? y : z >= x && z >= y ? z : "All numbers are equal";
+}
+console.log(maxOfThree(49, 50, 51));
+
+// 3. 
+function isCharAVowel(c) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    return vowels.includes(c);
+}
+console.log(isCharAVowel("a"));
+
+// 4.
+function sumArray(arr) {
+    let sum = 0;
+    for (const num of arr) {
+        sum += num;
+    }
+    return sum;
+}
+console.log(sumArray([2, 4, 5])); // should return 11
+
+// 5.
+function multiplyArray(arr) {
+    let product = 1;
+    for (const num of arr) {
+        product *= num;
+    }
+    return product;
+}
+console.log(multiplyArray([2, 4, 5])); // should return 40
+
+// 6.
+function numArgs(...args) {
+    return args.length;
+}
+console.log(numArgs(1,2,4,1,5,1,1)); // should return 7
+
+// 7.
+function reverseString(str) {
+    return str.split("").reverse().join(""); // turn string into array, reverse array, turn array into string
+}
+
+// 8.
+function longestStringInArray(arr) {
+    let longest = "";
+    for (const str of arr) {
+        if (str.length > longest.length) {
+            longest = str;
+        }
+    }
+    return longest;
+}
+console.log(longestStringInArray(["hello", "goodbye", "hi", "bye"])); // should return "goodbye"
+
+// 9. 
+function stringsLongerThan(arr, len) {
+    let returnArr = [];
+    for (const str of arr) {
+        if (str.length > len) {
+            returnArr.push(str);
+        }
+    }
+    return returnArr;       
+}
+console.log(stringsLongerThan(["hello", "goodbye", "hi", "bye"], 3)); // should return ["hello", "goodbye"]
